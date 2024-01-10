5a2b9c16774ae529c33aeebddcabd3fd4ba8dc74
Martin Palonsky <martinpalonsky98@gmail.com>
2024-01-10 20:34:21 -0300

    commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+/* operaciones básicas 
+
+let numeroA = 5;
+let numeroB = 3;
+const NUMEROC = 8;
+
+let resultadoSuma = numeroA + numeroB;
+let resultadoResta = numeroA - numeroB;
+let resultadoProducto = numeroB * NUMEROC;
+
+let textoA = "CODER";
+let textoB ="HOUSE";
+const BLANCO = " ";
+
+let resultadoA = textoA + textoB;
+let resultadoB = textoB + 1;
+let resultadoC = textoA + BLANCO + textoB;
+*/
+
+/* PROMPT 
+
+let nombreIngresado = prompt("Ingrese su nombre");
+*/
+
+/* CONSOLA
+
+console.log("Mensaje de prueba");
+let nombre = "Martín"
+console.log(nombre)
+*/
+
+/* ALERT 
+
+alert('Hola Martín!');
+*/
