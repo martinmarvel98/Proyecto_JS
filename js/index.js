34dc2684e86aa19716257a596d01c20359122422
Martin Palonsky <martinpalonsky98@gmail.com>
2024-01-10 21:39:09 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+alert('Hola mundo!');
+
 /* operaciones básicas 
 
 let numeroA = 5;
@@ -33,3 +35,24 @@ console.log(nombre)
 
 alert('Hola Martín!');
 */
+
+let numero = 15; //number
+let cadenaDeTexto = "MARTIN PALONSKY"; //string
+let booleanos = true; //boolean
+
+
+
+const NOMBRE_APELLIDO = "MARTIN PALONSKY";
+
+//declaracion
+
+//estoy reservando el nombre de la variable, y no le asignamos un valor
+//despues nosotros podemos inicianizarlo cuando queremos, en el const no se puede resignar, en el let, si
+//let edad;
+
+//inicializacion, proceso en el cual asignamos el valor inical de la variable
+let edad;
+
+edad = 25;
+
+//las variables siempre debe tener asignada un nombre que haga referencia al dato almacenado.
